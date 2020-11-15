6d1a5a5d83618db85dac67faf546ff77c425ef0c
dbehdgjs123 <dbehdgjs12345@gmail.com>
2020-11-15 23:24:10 +0900

    헤더 약간 작업

---
+++ client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <header-vue />
+      <!--<router-link to="/">Home</router-link> |-->
+      <!--<router-link to="/about">About</router-link>-->
+    </div>
+    <router-view />
+  </div>
+</template>
+<script>
+import HeaderVue from "./components/Header.vue";
+
+export default {
+  components: {
+    HeaderVue,
+  },
+};
+</script>
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+html {
+  height: 100%;
+}
+</style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header>
+      <div class="header-logo-box">
+        <img src="../assets/office.jpg" alt="logo" class="logo-img" />
+      </div>
+      <div class="header-top">
+        <a href="#"><i class="fas fa-moon"></i></a>
+      </div>
+      <div class="header-bottom">
+        <div class="header-bottom-left">
+          <div class="title">TODO</div>
+          <div class="today-date">-sep 15 2020</div>
+        </div>
+        <div class="header-bottom-right">
+          <i class="fas fa-calendar-alt"></i>
+          <i class="fas fa-clipboard-check"></i>
+        </div>
+      </div>
+    </header>
+    <main></main>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "./styles/header.scss";
+</style>
