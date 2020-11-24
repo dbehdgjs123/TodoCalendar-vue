d6effdee92a108a1cc462c72af6b30c35121b9c8
dbehdgjs123 <dbehdgjs12345@gmail.com>
2020-11-24 10:48:44 +0900

    유저기능 테스트 완료

--- client/src/store/modules/todoStore.js
+++ client/src/store/modules/todoStore.js
@@ -15,7 +15,10 @@ const todoStore = {
                         //생성된 날짜가 오늘이 포함되어 있지않으면 삭제한다.(현재 날짜의 todolist만 출력)
                         localStorage.removeItem(localStorage.key(i));
                     } else {
-                        state.todos.push(JSON.parse(localStorage.getItem(localStorage.key(i)))); //파싱해서 가져옴
+                        if(JSON.parse(localStorage.getItem(localStorage.key(i))).writer === this.getters.rootUser) {
+                            state.todos.push(JSON.parse(localStorage.getItem(localStorage.key(i)))); //파싱해서 가져옴
+                        }
+                        
                     }
                 }
             }
@@ -31,13 +34,14 @@ const todoStore = {
                 isCompleted: false,
                 todoItem: data,
                 createdDate: Vue.moment().format("YYYYMMDDHHmmss"),
+                writer: this.getters.rootUser
             };
             const isOverlap = state.todos.find((item) => item.todoItem === data); //중복된 값이 있으면 리턴 시킨다
             if (isOverlap) {
                 console.log("중복");
                 return;
             }
-            localStorage.setItem(data, JSON.stringify(obj));
+            localStorage.setItem(this.getters.rootItemKey + data, JSON.stringify(obj)); //key+data로 저장(중복방지)
             state.todos.push(obj); //배열에 객체 저장
             localStorage.setItem(this.getters.rootUser + this.getters.rootKey + state.today, JSON.stringify(state.todos));
             //history는 localstorage의 키는 고유의키값을 가지도록 유저의 아이디+고유키+날짜로 해준다.
@@ -45,13 +49,12 @@ const todoStore = {
         completeTodo(state, payload) {
             const { index } = payload; //완료 {item: 객체, index: 배열 순서}
             state.todos[index].isCompleted = !state.todos[index].isCompleted;
-            localStorage.setItem(state.todos[index].todoItem, JSON.stringify(state.todos[index])); //json 형태로 저장
-            //history의 배열에서도 바꿔준다.
-            localStorage.setItem(this.getters.rootUser + this.getters.rootKey + state.today, JSON.stringify(state.todos));
+            localStorage.setItem(this.getters.rootItemKey + state.todos[index].todoItem, JSON.stringify(state.todos[index])); //json 형태로 저장
+            localStorage.setItem(this.getters.rootUser + this.getters.rootKey + state.today, JSON.stringify(state.todos));//history의 배열에서도 바꿔준다.
         },
         removeTodo(state, payload) {
             const { item, index } = payload; //삭제 
-            localStorage.removeItem(item.todoItem);
+            localStorage.removeItem(this.rootItemKey + item.todoItem);
             state.todos.splice(index, 1); //인덱스에 해당하는 속성 하나 제거
             //history도 바꿔준다.
             //this.historyTodos.splice(index, 1); //인덱스에 해당하는 속성 하나 제거
--- client/src/store/modules/userStore.js
+++ client/src/store/modules/userStore.js
@@ -6,12 +6,15 @@ const userStore = {
     mutations: {},
     actions: {},
     getters: {
-        rootUser(state) {
+        rootUser(state) { //유저 아이디
             return state.userId;
         },
-        rootKey(state) {
+        rootKey(state) { //history,캘린더 구별용 키
             return state.uniqueKey;
         },
+        rootItemKey(state) { //todo아이템 구별용 키(다른 아이디를 가진사람이 똑같은 item을 입력할 수 있기때문에)
+            return state.userId.substring(1,7)
+        }
     },
 };
 export default userStore;
